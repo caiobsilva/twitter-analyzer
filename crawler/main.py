28c3551412eab80bcccbf29cb9cd3d9123d659e5
Caio Braga <caio.braga@enjoei.com.br>
2022-04-21 15:39:55 -0300

    refactor: implement 'search_tweet_data use case to improve readability

--- crawler/main.py
+++ crawler/main.py
@@ -1,6 +1,6 @@
 import tweepy, configparser
 from drivers.neo4j.client import Client
-from use_cases.create_tweet import CreateTweet
+from use_cases.search_tweet_data import SearchTweetData
 
 # read authentication credentials
 config = configparser.RawConfigParser()
@@ -15,25 +15,8 @@ bearer_token = config["twitter"]["bearer_token"]
 client = tweepy.Client(bearer_token=bearer_token)
 
 # get tweets
-tweets = client.search_recent_tweets(
-  query="bolsonaro",
-  tweet_fields=["created_at", "source", "referenced_tweets", "entities", "lang", "public_metrics", "reply_settings"],
-  user_fields=["name", "username", "created_at"],
-  max_results=10,
-  expansions=["author_id", "referenced_tweets.id", "referenced_tweets.id.author_id"]
-)
+tweets = SearchTweetData(client, "bolsonaro").execute()
 
-# create users and retweets dictionary
-users = { u["id"]: u for u in tweets.includes["users"] }
-retweets = { rt["id"]: rt for rt in tweets.includes["tweets"] }
-
-for tweet_data in tweets.data:
-  # get related data (parent tweets, users)
-  author_data = users[tweet_data.author_id] if users[tweet_data.author_id] else None
-  parent_id = tweet_data.referenced_tweets[0].id if tweet_data.referenced_tweets != None else None
-  parent_tweet = retweets[parent_id] if parent_id else None
-  parent_author = users[parent_tweet.author_id] if parent_tweet else None
-
-  tweet = CreateTweet(tweet_data, author_data, parent_tweet, parent_author).execute()
-
+# save data to db
+for tweet in tweets:
   neo4j.create(tweet, tweet.parent)
---
+++ crawler/use_cases/search_tweet_data.py
@@ -0,0 +1,53 @@
+from entities.tweet import Tweet
+from use_cases.create_tweet import CreateTweet
+from typing import List
+import tweepy
+
+class SearchTweetData:
+  EXPANSIONS = [
+    "author_id", "referenced_tweets.id", "referenced_tweets.id.author_id"
+  ]
+  TWEET_FIELDS = [
+    "created_at", "source", "referenced_tweets", "entities",
+    "lang", "public_metrics", "reply_settings"
+  ]
+  USER_FIELDS = [
+    "name", "username", "created_at"
+  ]
+
+  def __init__(self, client: tweepy.Client, query: str, max_results: int = 10):
+    self.client = client
+    self.query = query
+    self.max_results = max_results
+
+  def execute(self) -> List[Tweet]:
+    results = self.client.search_recent_tweets(
+      query = self.query,
+      max_results = self.max_results,
+      expansions = self.EXPANSIONS,
+      user_fields = self.USER_FIELDS,
+      tweet_fields = self.TWEET_FIELDS
+    )
+
+    # tweets, users and mentions (rts, quotes) come separated in different dictionaries
+    tweets_data = results.data
+    mentions = { m["id"]: m for m in results.includes["tweets"] }
+    users = { u["id"]: u for u in results.includes["users"] }
+
+    tweets = []
+    for tweet_data in tweets_data:
+      author_data = users[tweet_data.author_id]
+
+      if tweet_data.referenced_tweets is not None:
+        parent_id = tweet_data.referenced_tweets[0].id
+
+        parent_tweet = mentions[parent_id]
+        parent_author = users[parent_tweet.author_id]
+
+        tweet = CreateTweet(tweet_data, author_data, parent_tweet, parent_author).execute()
+      else:
+        tweet = CreateTweet(tweet_data, author_data).execute()
+
+      tweets.append(tweet)
+
+    return tweets
