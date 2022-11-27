1e98172c10b642735233bf23fd8db6ca9f4cd5f2
caiobsilva <caiobs.99@gmail.com>
2022-11-27 20:28:41 -0300

    improvement: calculate communities based on centrality outliers

--- application/use_cases/analyze_graph.py
+++ application/use_cases/analyze_graph.py
@@ -2,6 +2,8 @@ from typing import Any, List
 import networkx as nx
 import numpy as np
 
+import statistics
+
 class AnalyzeGraph:
   def __init__(self, nodes: Any, edges: Any) -> None:
     self.nodes = [n for n in nodes]
@@ -11,12 +13,11 @@ class AnalyzeGraph:
     graph = self._create_graph()
     relevant_graph = self._prune_irrelevant_nodes(graph)
 
-    deg_centrality = nx.degree_centrality(relevant_graph)
+    centrality = nx.pagerank(relevant_graph)
     positions = nx.spring_layout(relevant_graph, iterations=1000)
-    # positions = nx.kamada_kawai_layout(relevant_graph)
-    communities = self._get_node_communities(relevant_graph)
+    communities = self._get_node_communities(relevant_graph, centrality)
 
-    return self._aggregate_data(relevant_graph, deg_centrality, positions, communities)
+    return self._aggregate_data(relevant_graph, centrality, positions, communities)
 
 
   def _create_graph(self) -> nx.Graph:
@@ -31,12 +32,6 @@ class AnalyzeGraph:
     return g
 
   def _prune_irrelevant_nodes(self, graph: nx.Graph) -> nx.Graph:
-    # remove nodes that are part of subgraphs with < x connections
-    # for component in list(nx.connected_components(graph)):
-    #   if len(component) < 15:
-    #     for node in component:
-    #       graph.remove_node(node)
-
     # remove self loop edges
     graph.remove_edges_from(nx.selfloop_edges(graph))
 
@@ -45,15 +40,28 @@ class AnalyzeGraph:
 
     return relevant_subgraph
 
-  def _get_node_communities(self, graph: nx.Graph) -> dict:
-    # creates a 'supernode graph', merging nodes with a single edge
-    # to the connected node
+  def _get_node_communities(self, graph: nx.Graph, centrality_data: List) -> dict:
+    # get centrality outliers
+    influencer_nodes = self._get_outliers(centrality_data)
+
+    # creates a 'supernode graph', consisting of top influencers
     supernode_graph = graph.copy()
-    supernode_graph.remove_edges_from(nx.selfloop_edges(supernode_graph))
-    nodes_to_remove = [n for n in supernode_graph.nodes if len(list(supernode_graph.neighbors(n))) == 1]
+    keep_nodes = [k for k, v in centrality_data.items() if v >= influencer_nodes[-1]]
+    nodes_to_remove = [n for n in supernode_graph.nodes if n not in keep_nodes]
 
     for node in nodes_to_remove:
       supernode_graph.remove_node(node)
+
+    # create weighted edges for supernode graph.
+    # edge weight inversely proportional to path between nodes
+    for n in supernode_graph.nodes:
+      for m in supernode_graph.nodes:
+        if n == m: continue
+        
+        path_lenght = nx.algorithms.shortest_path_length(graph, source=n, target=m)
+        weight = 1 / path_lenght
+        
+        supernode_graph.add_weighted_edges_from([(n, m, weight)])
 
     # calculate communities for subgraph
     communities = nx.community.louvain_communities(supernode_graph)
@@ -63,10 +71,18 @@ class AnalyzeGraph:
       for node in community:
         node_community_dict[node] = i
 
-    # add removed nodes to their super's community
-    for node in nodes_to_remove:
-      neighbor = list(graph.neighbors(node))[0]
-      node_community_dict[node] = node_community_dict[neighbor]
+    # add removed nodes to their closest super's community
+    for node in graph:
+      path_lengths = {}
+      for supernode in supernode_graph:
+        if node == supernode: continue
+
+        path_lengths[supernode] = nx.algorithms.shortest_path_length(graph, source=node, target=supernode)
+
+      influencer_supernode = min(path_lengths, key=path_lengths.get)
+      if influencer_supernode is None: continue
+      
+      node_community_dict[node] = node_community_dict[influencer_supernode]
 
     return node_community_dict
 
@@ -88,3 +104,22 @@ class AnalyzeGraph:
       node["community"] = community
 
     return { "nodes": relevant_nodes, "edges": self.edges }
+
+  def _get_outliers(self, centralities: dict) -> List:
+    # calculate standard deviation
+    threshold = 3
+    centrality_data = list(centralities.values())
+    mean = np.mean(centrality_data)
+    std = np.std(centrality_data)
+    
+    outliers = []
+    for y in centrality_data:
+      z_score = (y - mean) / std 
+      if np.abs(z_score) > threshold:
+        outliers.append(y)
+
+    # filter small outliers
+    median = statistics.median(centrality_data)
+    filtered_outliers = [o for o in outliers if o > median]
+
+    return sorted(filtered_outliers, reverse=True)
